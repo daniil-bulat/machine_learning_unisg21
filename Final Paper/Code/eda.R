4e060b0ae1417aac58b345e73b8eb011ee65ffd7
daniil-bulat <56818590+daniil-bulat@users.noreply.github.com>
2021-06-02 17:49:34 +0200

    eda

---
+++ Final Paper/Code/eda.R
@@ -0,0 +1,75 @@
+# Explorative Data Analysis
+
+setwd("~/Desktop/Uni/FS21/Machine Learning/machine_learning_unisg21/Final Paper")
+
+# Data
+data = read.csv("Data/sentiment/reddit_sentiment.csv")
+data = data[,2:8]
+names(data) = c("text","score","num_comments","timestamp","sentences","scores","score_sum")
+
+
+
+#################################### Scores ####################################
+# Scores: 0 - 200
+ggplot(data=data, aes(score)) + 
+  geom_histogram(breaks=seq(0, 2000, by=1), 
+                 col="red", 
+                 fill="green", 
+                 alpha = .5) + 
+  labs(title="Histogram of Scores, 0 - 200", x="score", y="Count") + 
+  xlim(c(0,200)) + 
+  ylim(c(0,2000))
+
+# Scores: 0 - 1000
+ggplot(data=data, aes(score)) + 
+  geom_histogram(breaks=seq(0, 2000, by=1), 
+                 col="red", 
+                 fill="green", 
+                 alpha = .5) + 
+  labs(title="Histogram of Scores, 0 - 1000", x="score", y="Count") + 
+  xlim(c(0,1000)) + 
+  ylim(c(0,2000))
+
+#################################### Comments ##################################
+# Scores: 0 - 200
+ggplot(data=data, aes(num_comments)) + 
+  geom_histogram(breaks=seq(0, 2000, by=1), 
+                 col="blue", 
+                 fill="green", 
+                 alpha = .5) + 
+  labs(title="Histogram of Comments, 0 - 200", x="score", y="Count") + 
+  xlim(c(0,200)) + 
+  ylim(c(0,2000))
+
+# Scores: 0 - 1000
+ggplot(data=data, aes(num_comments)) + 
+  geom_histogram(breaks=seq(0, 2000, by=1), 
+                 col="blue", 
+                 fill="green", 
+                 alpha = .5) + 
+  labs(title="Histogram of Comments, 0 - 1000", x="score", y="Count") + 
+  xlim(c(0,1000)) + 
+  ylim(c(0,2000))
+
+#################################### Score Sum #################################
+# Scores: 0 - 200
+ggplot(data=data, aes(score_sum)) + 
+  geom_histogram(breaks=seq(-1, 1, by=0.01), 
+                 col="green", 
+                 fill="blue", 
+                 alpha = .5) + 
+  labs(title="Histogram of Sentiments, 0 - 200", x="score", y="Count") + 
+  xlim(c(-1,1)) + 
+  ylim(c(0,11000))
+
+#################################### Text Length ###############################
+# Scores: 0 - 200
+ggplot(data=data, aes(score_sum)) + 
+  geom_histogram(breaks=seq(-1, 1, by=0.01), 
+                 col="green", 
+                 fill="blue", 
+                 alpha = .5) + 
+  labs(title="Histogram of Sentiments, 0 - 200", x="score", y="Count") + 
+  xlim(c(-1,1)) + 
+  ylim(c(0,11000))
+
