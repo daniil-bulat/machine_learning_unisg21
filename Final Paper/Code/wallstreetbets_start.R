c73919f37ff1ddc1131c7796007e07c170c1d158
daniil-bulat <56818590+daniil-bulat@users.noreply.github.com>
2021-06-04 19:50:29 +0200

    ml

--- Final Paper/Code/wallstreetbets_start.R
+++ Final Paper/Code/wallstreetbets_start.R
@@ -12,6 +12,7 @@ setwd("~/Desktop/Uni/FS21/Machine Learning/machine_learning_unisg21/Final Paper"
 # Data
 data = read.csv("Data/sentiment/reddit_sentiment.csv")
 data = data[,2:8]
+data$texlen = nchar(data$text)
 
 #######
 # data_avg = data_avg[,-7]
@@ -48,10 +49,8 @@ data = data[,2:8]
 
 # How to filter for a stock
 gme_data = data %>%
-  select(text, score, num_comments, timestamp, sentences, scores, scores_avg) %>%   
+  select(text, score, num_comments, timestamp, sentences, scores, scores_avg,texlen) %>%   
   filter(str_detect(text, "GME|game stop|gme|Game Stop"))
-
-
 
 # GME closing values
 gme = read.csv("Data/stocks/GME_jan_may_21.csv")
@@ -61,5 +60,65 @@ gme = gme[,-(2:4)]
 # percentage change for gme stock
 gme = gme %>%
   mutate(pct_change = (Close/lag(Close) - 1) * 100)
+
+############################ Machine Learning ##################################
+# remove unnecesary rows
+ml_data = gme_data[,-(5:6)]
+ml_data = ml_data[,-(1)]
+
+# join sentiment and stock data
+ml_join_data = left_join(gme, ml_data,
+                    by = c("Date" = "timestamp"))
+
+ml_join_data = ml_join_data[-(1:73),-2]
+
+# give sentiments weight
+ml_join_data$weighted_sent = ml_join_data$scores_avg *(ml_join_data$score + ml_join_data$num_comments)
+
+# Filter by Day
+gme_final_data = ml_join_data %>%
+  group_by(Date) %>% summarise(mean(weighted_sent),mean(pct_change),mean(texlen))
+
+names(gme_final_data)[names(gme_final_data) == "mean(weighted_sent)"] = "Sentiment"
+names(gme_final_data)[names(gme_final_data) == "mean(pct_change)"] = "Pct_Change"
+
+############################## Logistic Regression #############################
+
+# Split the data into training and test sample (80%-20%)
+data.Train = gme_final_data[1:68,]
+data.Test = gme_final_data[69:85,]
+
+# Regression on the training data
+gme_model = lm(Pct_Change ~ Sentiment, data=data.Train)
+summary(gme_model) 
+
+# coefficients:
+coefs=gme_model$coefficients      
+coefs
+
+#Prediction with the model on the test data
+y_hat=coefs[2]*data.Test$Sentiment+coefs[1]
+
+#Error vector
+err=y_hat-data.Test$Pct_Change
+
+#Average square deviation
+sum(err^2)/length(err)
+
+# Plot Training Data
+plot(data.Train$Pct_Change,data.Train$Sentiment)
+abline(gme_model)
+
+
+
+
+
+
+# Random Forrest
+
+
+
+
+
 
 
