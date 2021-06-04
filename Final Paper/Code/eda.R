7cb106c40c82a41b058aaa5fe2eef3a3752de6cd
daniil-bulat <56818590+daniil-bulat@users.noreply.github.com>
2021-06-04 18:17:12 +0200

    eda

--- Final Paper/Code/eda.R
+++ Final Paper/Code/eda.R
@@ -6,9 +6,11 @@ setwd("~/Desktop/Uni/FS21/Machine Learning/machine_learning_unisg21/Final Paper"
 data = read.csv("Data/sentiment/reddit_sentiment.csv")
 data = data[,2:8]
 
+plot_data = data
+
 #################################### Scores ####################################
 # Scores: 0 - 200
-ggplot(data=data, aes(score)) + 
+ggplot(data=plot_data, aes(score)) + 
   geom_histogram(col="red", 
                  fill="green", 
                  alpha = .5) + 
@@ -17,7 +19,7 @@ ggplot(data=data, aes(score)) +
   ylim(c(0,2000))
 
 # Scores: 0 - max
-ggplot(data=data, aes(score)) + 
+ggplot(data=plot_data, aes(score)) + 
   geom_histogram(col="red", 
                  fill="green", 
                  alpha = .5) + 
@@ -27,7 +29,7 @@ ggplot(data=data, aes(score)) +
 
 #################################### Comments ##################################
 # Comments: 0 - 200
-ggplot(data=data, aes(num_comments)) + 
+ggplot(data=plot_data, aes(num_comments)) + 
   geom_histogram(breaks=seq(0, 2000, by=1), 
                  col="blue", 
                  fill="green", 
@@ -37,27 +39,27 @@ ggplot(data=data, aes(num_comments)) +
   ylim(c(0,2000))
 
 # Comments: 0 - 10000
-ggplot(data=data, aes(num_comments)) + 
+ggplot(data=plot_data, aes(num_comments)) + 
   geom_histogram(#breaks=seq(0, 2000, by=1), 
-                 col="blue", 
-                 fill="green", 
-                 alpha = .5) + 
+    col="blue", 
+    fill="green", 
+    alpha = .5) + 
   labs(title="Histogram of Comments, 0 - 10'000", x="Comments", y="Count") + 
   xlim(c(0,10000)) + 
   ylim(c(0,2000))
 
 #################################### Score Mean #################################
-ggplot(data=data, aes(scores_avg)) + 
+ggplot(data=plot_data, aes(scores_avg)) + 
   geom_histogram(col="green", 
                  fill="blue", 
                  alpha = .5) + 
   labs(title="Histogram of Sentiments", x="Sentiment", y="Count")
 
 #################################### Text Length ###############################
-data$texlen = nchar(data$text)
+plot_data$texlen = nchar(plot_data$text)
 
 # Text Length: 0 - 200
-ggplot(data=data, aes(texlen)) + 
+ggplot(data=plot_data, aes(texlen)) + 
   geom_histogram(col="green", 
                  fill="blue", 
                  alpha = .5) + 
@@ -66,7 +68,7 @@ ggplot(data=data, aes(texlen)) +
 
 
 # Text Length: 0 - max
-ggplot(data=data, aes(texlen)) + 
+ggplot(data=plot_data, aes(texlen)) + 
   geom_histogram(breaks=seq(0, 41000, by=10),
                  col="green", 
                  fill="blue", 
@@ -77,25 +79,24 @@ ggplot(data=data, aes(texlen)) +
 
 ########################### Day / Sentiment ####################################
 weekdays = c("Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag","Samstag", "Sonntag")
-data$day = weekdays(as.Date(data$timestamp))
-mean(data$scores_avg)
+plot_data$day = weekdays(as.Date(plot_data$timestamp))
+mean(plot_data$scores_avg)
 
 # Boxplot
-p = ggplot(data = data, aes(x=day, y=scores_avg, fill=day)) + 
+p = ggplot(data = plot_data, aes(x=day, y=scores_avg, fill=day)) + 
   geom_boxplot() + 
   ylim(c(-1,1))
 p + facet_wrap( ~ day, scales="free")
 
 # Boxplot with correct order
-data$day = factor(data$day , levels=weekdays)
+plot_data$day = factor(plot_data$day , levels=weekdays)
 
-boxplot(data$scores_avg ~ data$day , col=rgb(0.2,0.8,0.6,0.8,1) , ylab="mean sent score" , 
+boxplot(plot_data$scores_avg ~ plot_data$day , col=rgb(0.2,0.8,0.6,0.8,1) , ylab="mean sent score" , 
         xlab="Weekdays")
 
 ################################################################################
 # Time Series of Sentiment over Weekdays
-data$sent = ifelse(data$scores_avg > 0,"positiv", ifelse(data$scores_avg==0, "neutral", "negativ"))
-plot_data = data
+plot_data$sent = ifelse(plot_data$scores_avg > 0,"positiv", ifelse(plot_data$scores_avg==0, "neutral", "negativ"))
 
 day_score = data.frame(matrix(ncol = 3, nrow = 0))
 x = c("pos", "neg", "day")
